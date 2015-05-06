a8978095bb775c35289dcfb602e3c11fe1bc199a
parker <ameriod@gmail.com>
2015-05-06 18:42:55 -0400

    changed styling around, add a copy right

--- lib/src/main/java/me/parkerwilliams/stringpicker/StringPicker.java
+++ lib/src/main/java/me/parkerwilliams/stringpicker/StringPicker.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright (C) 2008 Parker Williams
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.parkerwilliams.stringpicker;
 
 import android.content.Context;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 
@@ -28,8 +45,8 @@ public class StringPicker extends NumberPickerTextView {
         super(context, attrs, defStyle);
     }
 
-    public static interface OnValueChangeListener {
-        public void onValueChanged(StringPicker picker, String oldVal, String newValue);
+    public interface OnValueChangeListener {
+        void onValueChanged(StringPicker picker, String oldVal, String newValue);
     }
 
     public void setOnValueChangeListener(OnValueChangeListener listener) {
@@ -56,11 +73,15 @@ public class StringPicker extends NumberPickerTextView {
         setValue(current);
     }
 
+    @Nullable
     public String getCurrentValue() {
-        return mValues[getCurrent()];
+        if (mValues != null) {
+            return mValues[getCurrent()];
+        }
+        return null;
     }
 
-    public void setValues(final String[] values) {
+    public void setValues(@NonNull String[] values) {
         mValues = values;
         setMaxValue(values.length - 1);
         setMinValue(0);
@@ -68,17 +89,19 @@ public class StringPicker extends NumberPickerTextView {
         setDisplayedValues(values);
     }
 
-    public void setValues(final List<String> values) {
+    public void setValues(@NonNull List<String> values) {
         mValues = values.toArray(new String[values.size()]);
         setValues(mValues);
     }
 
     public void setCurrent(@NonNull String currentValue) {
-        for (int i = 0, size = mValues.length; i < size; i++) {
-            String pickerValue = mValues[i];
-            if (TextUtils.equals(currentValue, pickerValue)) {
-                setCurrent(i);
-                break;
+        if (mValues != null) {
+            for (int i = 0, size = mValues.length; i < size; i++) {
+                String pickerValue = mValues[i];
+                if (TextUtils.equals(currentValue, pickerValue)) {
+                    setCurrent(i);
+                    break;
+                }
             }
         }
     }
